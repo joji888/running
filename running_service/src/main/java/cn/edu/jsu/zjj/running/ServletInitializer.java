e5e681058f49d3d84ad6107c82ca9875916a60d1
欧阳瑶峰 <15226382287@163.com>
2021-12-26 08:49:00 +0800

    项目文件整合

---
+++ running_service/src/main/java/cn/edu/jsu/zjj/running/ServletInitializer.java
@@ -0,0 +1,12 @@
+package cn.edu.jsu.zjj.running;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RunningApplication.class);
+    }
+}
---
+++ running_service/src/main/java/cn/edu/jsu/zjj/running/config/MyConfig.java
@@ -0,0 +1,19 @@
+package cn.edu.jsu.zjj.running.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("*")
+                .allowedMethods("GET","PUT","POST","DELETE")
+                .allowedOriginPatterns("*")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+}

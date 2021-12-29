413bd40839488aeeb58d1b4da27b59f561587a49
LMY <1148447035@qq.com>
2021-12-29 16:57:53 +0800

    提交

--- running_app/src/views/login.vue
+++ running_app/src/views/login.vue
@@ -1,21 +1,32 @@
 <template>
     <div class="loginMain">
-        <img class="loginImg" src="../assets/loginImg.png">
-        <el-form :model="users" :rules="rules" ref="users" class="loginForm">
-            <el-form-item prop="acc">
-                <el-input v-model="users.acc" type="text"     autocomplete="off" placeholder="账号" style="background-color: transparent"><i slot="prefix" class="el-input__icon el-icon-user"></i></el-input>
-            </el-form-item>
-            <el-form-item prop="pwd">
-                <el-input v-model="users.pwd" type="password" autocomplete="off" placeholder="密码" @keyup.enter="login('users')"><i slot="prefix" class="el-input__icon el-icon-lock"></i></el-input>
-            </el-form-item>
 
-            <el-form-item>
-                <el-button type="primary" class="loginBut" @click="login('users')" >登陆</el-button>
+        <div style="width:85%">
+            <div style="font-size: 48px;font-weight: bold">登录</div>
+            <div style="font-size: 12px;font-weight: bold">还没有账户？<span style="color: gold">立即注册</span></div>
+        </div>
 
-            </el-form-item>
-            <span style="color: white;font-size: 14px">跑腿平台--吉首大学张家界学院</span>
-            <br><span style="color: white;font-size: 14px">2021-现在 版权所有</span>
-        </el-form>
+        <img
+                style="margin-top: 40px ; margin-bottom: 40px"
+                width="150"
+                height="150"
+                src="../assets/loginImg.png"
+        />
+
+        <div style="width: 78%">帐号</div>
+        <van-field type="text" placeholder="请输入账号" style="width: 85%"/><br>
+
+        <div style="width: 78%">密码</div>
+        <van-field type="password" placeholder="请输入密码" style="width: 85%"/>
+        <br>
+        <div style="font-size:14px;width: 78%">
+            <span style="color:orange">短信快捷登录</span>
+            <span style="color: orange;float: right">忘记密码</span>
+        </div>
+
+        <van-button round type="info" style="width:85%;margin-top: 30px;" >登录</van-button>
+
+
     </div>
 </template>
 
@@ -24,52 +35,9 @@
         name: "login",
         data(){
             return {
-                users:{
-                    acc:"admin",
-                    pwd:""
-                },
-                rules: {
-                    acc: [
-                        { required: true, message: '请输人账号', trigger: 'blur' }
-                    ],
-                    pwd: [
-                        { required: true, message: '请输入密码', trigger: 'blur' }
-                    ]
-                }
             }
         },
         methods:{
-            login(users){
-                let _this=this;
-                this.$refs[users].validate((valid) => {
-                    if (valid) {
-                        console.log(this.users);
-                        let obj=this.users;
-                        this.$http.get('/admin/login',{
-                            params:obj
-                        }).then(function (res) {
-                            console.log(res);
-                            if (res.data.code===200){
-                                if (res.data.data!=null){
-                                    console.log("登陆成功 token:\n"+res.data.data['token'])
-                                    let time=new Date();
-                                    console.log(time.getTime())
-                                    localStorage.setItem("token",JSON.stringify({
-                                        'admin':res.data.data['admin'],
-                                        'token':res.data.data['token'],
-                                        'time':time.getTime()+1000*60*60*3
-                                    }));
-                                    _this.$http.defaults.headers.common['token'] = res.data.data['token'];
-                                    _this.$router.push({path:'/',query:{}});
-                                }
-                            }
-                        })
-                    } else {
-                        console.log('error submit!!');
-                        return false;
-                    }
-                });
-            }
         }
     }
 </script>
@@ -82,7 +50,8 @@
         align-items: center;
         flex-direction: column;
         justify-content: center;
-        background-color: rgb(20, 26, 72)
+        /*background-color: rgba(25, 186, 250, 0.99)*/
+        background-color: white;
     }
     .loginImg{
         width: 200px;
---
+++ running_app/src/views/register.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="loginMain">
+
+        <div style="width:85%">
+            <div style="font-size: 48px;font-weight: bold">注册</div>
+            <div style="font-size: 12px;font-weight: bold">已有帐户？<span style="color: gold">立即登录</span></div>
+        </div>
+
+        <div style="width: 78%;margin-top: 40px">手机号</div>
+        <van-field type="text" placeholder="请输入手机号" style="width: 85%"/><br>
+
+        <div style="width: 78%">邮箱号</div>
+        <van-field type="text" placeholder="请输入邮箱号" style="width: 85%"/><br>
+
+        <div style="width: 78%">邮箱号验证码</div>
+        <van-field type="text" placeholder="请输入邮箱号验证码" style="width: 85%">
+            <template #button>
+                <van-button size="small" type="primary" style="background-color:#1989fa" >发送验证码</van-button>
+            </template>
+        </van-field><br>
+
+
+        <div style="width: 78%">密码</div>
+        <van-field type="password" placeholder="请输入密码" style="width: 85%">
+
+        </van-field>
+
+        <div style="width: 78%">确认密码</div>
+        <van-field type="password" placeholder="请再次输入密码" style="width: 85%"/>
+        <br>
+       
+
+        <van-button round type="info" style="width:85%;margin-top: 30px;" >注册</van-button>
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "login",
+        data(){
+            return {
+            }
+        },
+        methods:{
+        }
+    }
+</script>
+
+<style scoped>
+    .loginMain{
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        justify-content: center;
+        /*background-color: rgba(25, 186, 250, 0.99)*/
+        background-color: white;
+    }
+    .loginImg{
+        width: 200px;
+        height: 200px;
+        margin-top: -200px;
+        border-radius: 50%;
+        margin-bottom: 30px;
+    }
+    .loginForm{
+        width: 300px;
+        text-align: center;
+    }
+    .loginBut{
+        width: 100%;
+    }
+
+</style>
